aae366dcbbd37afc4d4fe0fd893930145b847fec
[Aidana] <[ajdanamaratova20@gmail.com]>
2026-09-23 09:22:30 +0600

    главный путь будет Swagger

--- config/urls.py
+++ config/urls.py
@@ -14,12 +14,12 @@ from drf_spectacular.views import SpectacularSwaggerView
 
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("", admin.site.urls),
     path("api/", include("catalog.urls")),
     path('api/', include('users.urls')),
     path('api/token/', TokenObtainPairView.as_view()),
     path('api/token/refresh/', TokenRefreshView.as_view()),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/',SpectacularSwaggerView.as_view(url_name='schema'),name='swagger-ui'),
-    path('', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    #path('', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ]
