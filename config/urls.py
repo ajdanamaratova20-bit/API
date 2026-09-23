67c7a69e0914ff0439b639fcdc467d7375282cca
[Aidana] <[ajdanamaratova20@gmail.com]>
2026-09-23 09:31:48 +0600

    главный путь будет Swagger

--- config/urls.py
+++ config/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
     path('api/token/refresh/', TokenRefreshView.as_view()),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/',SpectacularSwaggerView.as_view(url_name='schema'),name='swagger-ui'),
-    #path('', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ]
